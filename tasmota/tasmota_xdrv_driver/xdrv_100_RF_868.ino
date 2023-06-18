3ba39735418140d9cb1c253294a6bc051807cffa
pindrei <MaikBollmann@gmail.com>
2023-06-18 07:25:07 +0200

    Test

--- tasmota/tasmota_xdrv_driver/xdrv_100_RF_868.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_100_RF_868.ino
@@ -45,8 +45,8 @@
 #define PIN_RECEIVER_RST  2
 #define NUM_SENSORS 1
 //#include "WeatherSensorCfg.h"
-#include "WeatherSensor.h"
 
+#include "cc1101.h"
 #define SYNC_WORD 199
 
 #define Lowpulse         400
@@ -58,8 +58,9 @@ const char kJaroliftCommands[] PROGMEM = "Keeloq|" // prefix
 void (* const jaroliftCommand[])(void) PROGMEM = {
   &CmndSendRaw, &CmdSendButton, &CmdSet};
 
-//CC1101 cc1101;
-WeatherSensor weatherSensor;
+CC1101 cc1101;
+
+
 struct JAROLIFT_DEVICE {
   int device_key_msb       = 0x0; // stores cryptkey MSB
   int device_key_lsb       = 0x0; // stores cryptkey LSB
@@ -74,86 +75,26 @@ struct JAROLIFT_DEVICE {
 } jaroliftDevice;
 
 void init_RF868(){
-     weatherSensor.begin();
+  jaroliftDevice.port_tx = Pin(GPIO_CC1101_GDO2);              // Output port for transmission
+  jaroliftDevice.port_rx = Pin(GPIO_CC1101_GDO0);              // Input port for reception
+
+  DEBUG_DRIVER_LOG(PSTR("RF868: cc1101.init()"));
+  delay(100);
+  cc1101.init();
+  AddLog(LOG_LEVEL_DEBUG_MORE, PSTR("RF868: CC1101 done"));
+  cc1101.setSyncWord(SYNC_WORD, false);
+  cc1101.setCarrierFreq(CFREQ_433);
+  cc1101.disableAddressCheck();
+
+  pinMode(jaroliftDevice.port_tx, OUTPUT);
+  pinMode(jaroliftDevice.port_rx, INPUT_PULLUP);
+
+  jaroliftDevice.serial = Settings->keeloq_serial;
+  jaroliftDevice.count = Settings->keeloq_count;
+  //GenerateDeviceCryptKey();
 }
 void loop_RF868(){
-  weatherSensor.clearSlots(); 
-    // This example uses only a single slot in the sensor data array
-    int const i=0;
-
-    // Clear all sensor data
-    weatherSensor.clearSlots();
-
-    // Tries to receive radio message (non-blocking) and to decode it.
-    // Timeout occurs after a small multiple of expected time-on-air.
-    int decode_status = weatherSensor.getMessage();
-    AddLog(LOG_LEVEL_DEBUG_MORE, PSTR("RF868: Message %02d"),decode_status);
-/*
-    if (decode_status == DECODE_OK) {
-    
-      Serial.printf("Id: [%8X] Typ: [%X] Battery: [%s] ",
-          weatherSensor.sensor[i].sensor_id,
-          weatherSensor.sensor[i].s_type,
-          weatherSensor.sensor[i].battery_ok ? "OK " : "Low");
-      #ifdef BRESSER_6_IN_1
-          Serial.printf("Ch: [%d] ", weatherSensor.sensor[i].chan);
-      #endif
-      if (weatherSensor.sensor[i].temp_ok) {
-          Serial.printf("Temp: [%5.1fC] ",
-              weatherSensor.sensor[i].temp_c);
-      } else {
-          Serial.printf("Temp: [---.-C] ");
-      }
-      if (weatherSensor.sensor[i].humidity_ok) {
-     //     Serial.printf("Hum: [%3d%%] ",
-     //         weatherSensor.sensor[i].humidity);
-      }
-      else {
-      //    Serial.printf("Hum: [---%%] ");
-      }
-      if (weatherSensor.sensor[i].wind_ok) {
-      //    Serial.printf("Wind max: [%4.1fm/s] Wind avg: [%4.1fm/s] Wind dir: [%5.1fdeg] ",
-      //            weatherSensor.sensor[i].wind_gust_meter_sec,
-      //            weatherSensor.sensor[i].wind_avg_meter_sec,
-      //            weatherSensor.sensor[i].wind_direction_deg);
-      } else {
-     //     Serial.printf("Wind max: [--.-m/s] Wind avg: [--.-m/s] Wind dir: [---.-deg] ");
-      }
-      if (weatherSensor.sensor[i].rain_ok) {
-       //   Serial.printf("Rain: [%7.1fmm] ",  
-       //       weatherSensor.sensor[i].rain_mm);
-      } else {
-        //  Serial.printf("Rain: [-----.-mm] "); 
-      }
-      if (weatherSensor.sensor[i].moisture_ok) {
-      //    Serial.printf("Moisture: [%2d%%] ",
-       //       weatherSensor.sensor[i].moisture);
-      }
-      else {
-          Serial.printf("Moisture: [--%%] ");
-      }
-      #if defined BRESSER_6_IN_1 || defined BRESSER_7_IN_1
-      if (weatherSensor.sensor[i].uv_ok) {
-     //     Serial.printf("UV index: [%1.1f] ",
-      //        weatherSensor.sensor[i].uv);
-      }
-      else {
-      //    Serial.printf("UV index: [-.-%%] ");
-      }
-      #endif
-      #ifdef BRESSER_7_IN_1
-      if (weatherSensor.sensor[i].light_ok) {
-       //   Serial.printf("Light (Klux): [%2.1fKlux] ",
-        //      weatherSensor.sensor[i].light_klx);
-      }
-      else {
-      //    Serial.printf("Light (lux): [--.-Klux] ");
-      }
-      #endif      
-     // Serial.printf("RSSI: [%5.1fdBm]\n", weatherSensor.sensor[i].rssi);
-    } // if (decode_status == DECODE_OK)
-    //delay(100);
-*/
+ 
 }
 /*********************************************************************************************\
  * Interface
@@ -173,10 +114,9 @@ bool Xdrv100(uint32_t function)
       break;
     case FUNC_INIT:
    //   KeeloqInit();
-      AddLog(LOG_LEVEL_INFO, PSTR("RF868: Init start"));
-     
+      
       init_RF868();
-       AddLog(LOG_LEVEL_INFO, PSTR("RF868: Init done"));
+      
      
       break;
     case FUNC_WEB_SENSOR:
